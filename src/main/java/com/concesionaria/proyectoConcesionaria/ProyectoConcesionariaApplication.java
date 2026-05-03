c624b036b77c661e45d379cf358a7772677c873b
williams thomas tapia navarrete <wil.tapia@duocuc.cl>
2026-05-02 21:44:40 -0400

    feat: estructura inicial de 10 microservicios para la concesionaria

---
+++ src/main/java/com/concesionaria/proyectoConcesionaria/ProyectoConcesionariaApplication.java
@@ -0,0 +1,13 @@
+package com.concesionaria.proyectoConcesionaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoConcesionariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoConcesionariaApplication.class, args);
+	}
+
+}
